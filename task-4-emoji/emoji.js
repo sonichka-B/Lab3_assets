0921228e4a5c518a4ba2624f793f081a4b6a0f03
Bordachenko Sofiia <s.bordachenko@gmail.com>
2026-05-27 19:02:46 +0300

    feat: practice task 4
    Signed-off-by: Bordachenko Sofiia <s.bordachenko@gmail.com>

--- task-4-emoji/emoji.js
+++ task-4-emoji/emoji.js
@@ -33,24 +33,40 @@ const dictionary = {
  * // "сьогодні ☀️ і 😊"
  */
 function translate(text) {
-  // TODO
+  let correctText = text.toLowerCase().split(/([^а-яіїєґa-z]+)/i);
+  const result = [];
+  for (let i = 0; i < correctText.length; i++) {
+    if(dictionary[correctText[i]]){
+      result.push(dictionary[correctText[i]]);
+    }
+    else
+      result.push(correctText[i]);
+  }
+  return result.join("");
 }
 
 /**
  * Зворотний переклад: емодзі → слово (українською).
  */
 function translateReverse(text) {
-  // TODO
+  let dividedText = text.split(" ");
+  const result = [];
+  for (let i = 0; i < dividedText.length; i++) {
+    const foundWord = Object.keys(dictionary).find((value) => dictionary[value]===dividedText[i]);
+    if(foundWord) result.push(foundWord);
+    else result.push(dividedText[i]);
+  }
+  return result.join(" ");
 }
 
 // ============================================================
 // Тестові кейси
 // ============================================================
-// console.log(translate("сьогодні сонце і усмішка"));
+console.log(translate("сьогодні сонце і усмішка"));
 // // "сьогодні ☀️ і 😊"
 //
-// console.log(translate("СОНЦЕ і Хмара. Дощ?"));
+console.log(translate("СОНЦЕ і Хмара. Дощ?"));
 // // "☀️ і ☁️. 🌧️?"
 //
-// console.log(translateReverse("☀️ і ☁️"));
+console.log(translateReverse("☀️ і ☁️"));
 // // "сонце і хмара"
